75ffc5edee5a8ea86c39d927f42ad5677cf5be0f
DeepashaDahiya <144469025+DeepashaDahiya@users.noreply.github.com>
2025-08-03 02:47:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Optional: cute alert for the contact form
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for your sweet message 💖 I'll get back to you soon!");
+  });
+  
